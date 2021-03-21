b582d60e75d3ca15070df36a40f94853c1d307a3
Olivier Pichon <op@dzangolab.com>
2021-03-21 16:43:25 +0700

    fix: fix provider source

--- floating-ip/providers.tf
+++ floating-ip/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source  = "hashicorp/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = ">= 1.22.2"
     }
   }
