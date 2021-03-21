795ee166715d2e92d692786899edb9e7a5e47c4a
Olivier Pichon <op@dzangolab.com>
2021-03-21 15:08:06 +0700

    chore: remove ignored content

--- droplet/providers.tf
+++ droplet/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 1.22.2"
+      version = ">= 1.22.2"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.4.0"
+      version = ">= 1.4.0"
     }
     template = {
       source  = "hashicorp/template"
-      version = "~> 2.1.2"
+      version = ">= 2.1.2"
     }
   }
   required_version = ">= 0.13.0"
