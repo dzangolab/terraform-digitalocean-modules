26710f894a0412632ada670d567a1f3bae178f36
Olivier Pichon <op@dzangolab.com>
2021-03-21 15:20:13 +0700

    fix(providers): fix provider source

--- droplet/providers.tf
+++ droplet/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source  = "digitalocean/digitalocean"
+      source  = "hashicorp/digitalocean"
       version = ">= 1.22.2"
     }
     local = {
