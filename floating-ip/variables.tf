d0cfd582cc5a866b4572a220c1b5066b1d811f68
Olivier Pichon <203745+opichon@users.noreply.github.com>
2021-03-29 17:39:10 +0700

    Main (#15)
    * feat(floating-ip): add support for provisioning multiple ips (#12)
    
    * fix(floating-ip): fix var name
    
    * fix(floating_ip): fix var name

--- floating-ip/variables.tf
+++ floating-ip/variables.tf
@@ -1,10 +1,3 @@
-variable "count" {
-  default     = 1
-  description = "The number of loating IPs to create"
-  type        = number
-}
-
-
 variable "digitalocean_token" {
   description = "Digital Ocean token"
   type        = string
@@ -14,6 +7,12 @@ variable "droplet_id" {
   default     = null
   description = "(Optional) The ID of Droplet that the Floating IP will be assigned to."
   type        = string
+}
+
+variable "ip_count" {
+  default     = 1
+  description = "The number of loating IPs to create"
+  type        = number
 }
 
 variable "region" {
