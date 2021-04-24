6448738ffa854cc27dce33416c56b1a28f7ea3ef
Olivier Pichon <203745+opichon@users.noreply.github.com>
2021-04-24 19:23:07 +0700

    0.3 firewall (#25)
    * feat(firewall): add support for source addresses in inbound rules
    
    * feat(firewall): add support for destination addresses in outbound rules
    
    * feat(firewall): add support for destination addresses in outbound rules
    
    * feat(firewall): add support for destination addresses in outbound rules
    
    * feat(firewall): add support for destination addresses in outbound rules
    
    * feat(firewall): add support for destination addresses in outbound rules
    
    * feat(firewall): add support for destination addresses in outbound rules

--- firewall/variables.tf
+++ firewall/variables.tf
@@ -10,8 +10,25 @@ variable "droplet_ids" {
 }
 
 variable "inbound_rules" {
+  default = [
+    {
+      port_range                = ""
+      protocol                  = ""
+      source_addresses          = []
+      source_droplet_ids        = []
+      source_load_balancer_uids = []
+      source_tags               = []
+    }
+  ]
   description = "The inbound and outbound access rule block for the Firewall."
-  type        = list(any)
+  type = list(object({
+    port_range                = string
+    protocol                  = string
+    source_addresses          = list(string)
+    source_droplet_ids        = list(string)
+    source_load_balancer_uids = list(string)
+    source_tags               = list(string)
+  }))
 }
 
 variable "name" {
@@ -20,8 +37,25 @@ variable "name" {
 }
 
 variable "outbound_rules" {
+  default = [
+    {
+      destination_addresses          = []
+      destination_droplet_ids        = []
+      destination_load_balancer_uids = []
+      destination_tags               = []
+      port_range                     = ""
+      protocol                       = ""
+    }
+  ]
   description = "The inbound and outbound access rule block for the Firewall."
-  type        = list(any)
+  type = list(object({
+    destination_addresses          = list(string)
+    destination_droplet_ids        = list(string)
+    destination_load_balancer_uids = list(string)
+    destination_tags               = list(string)
+    port_range                     = string
+    protocol                       = string
+  }))
 }
 
 variable "tags" {
