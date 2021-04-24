ba3d5c84eb1cefe28406c2cc30c054e4851d1f13
Olivier Pichon <op@dzangolab.com>
2021-04-24 18:29:45 +0700

    feat(firewall): add support for destination addresses in outbound rules

--- firewall/main.tf
+++ firewall/main.tf
@@ -6,7 +6,7 @@ resource "digitalocean_firewall" "this" {
   dynamic "inbound_rule" {
     for_each = var.inbound_rules
     content {
-      port_range                = inbound_rule.value["port_range"]
+      port_range                = inbound_rule.value["port_range"] == "1-65535" ? inbound_rule.value["port_range"]
       protocol                  = inbound_rule.value["protocol"]
       source_addresses          = inbound_rule.value["source_addresses"]
       source_droplet_ids        = inbound_rule.value["source_droplet_ids"]
@@ -22,7 +22,7 @@ resource "digitalocean_firewall" "this" {
       destination_droplet_ids        = outbound_rule.value["destination_droplet_ids"]
       destination_load_balancer_uids = outbound_rule.value["destination_load_balancer_uids"]
       destination_tags               = outbound_rule.value["destination_tags"]
-      port_range                     = outbound_rule.value["port_range"]
+      port_range                     = outbound_rule.value["port_range"] == "1-65535" ? outbound_rule.value["port_range"]
       protocol                       = outbound_rule.value["protocol"]
     }
   }
