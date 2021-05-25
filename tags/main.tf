127ec62c5513528bf8d5bad1b916c55801561a3e
Sabir Manandhar <sabir@dzangolab.com>
2021-05-25 11:11:06 +0545

    feature: create tags that do not exist

--- tags/main.tf
+++ tags/main.tf
@@ -1,4 +1,15 @@
+data "digitalocean_tags" "existing_tags" {
+  filter {
+    key    = "name"
+    values = var.names
+  }
+}
+
+locals {
+  new_tags = tolist(setsubtract(var.tags, data.digitalocean_tags.existing_tags.tags[*].name))
+}
+
 resource "digitalocean_tag" "this" {
-  count = length(var.names)
-  name  = var.names[count.index]
+  count = length(local.new_tags)
+  name  = local.new_tags[count.index]
 }
