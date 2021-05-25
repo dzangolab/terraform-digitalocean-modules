0f728c45a0294f78cbe1845e02619872c98dda34
Sabir Manandhar <sabir@dzangolab.com>
2021-05-25 12:48:04 +0545

    chore(tags): change deescription of output

--- tags/outputs.tf
+++ tags/outputs.tf
@@ -1,9 +1,9 @@
 output "ids" {
-  description = "The id of the new tags."
+  description = "The list of id of the new tags."
   value       = digitalocean_tag.this.*.id
 }
 
 output "names" {
-  description = "The name of the tags."
+  description = "The list of name of the tags."
   value       = tolist(setunion(digitalocean_tag.this.*.name, data.digitalocean_tags.existing_tags.tags.*.name))
 }
