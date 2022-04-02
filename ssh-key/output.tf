f0e17105a88cbbfc18bd1d8d517e528e9339b47b
Olivier Pichon <op@dzango.com>
2022-04-02 16:54:32 +0700

    fix[ssh_key]: add support for multiple keys

--- ssh-key/output.tf
+++ ssh-key/output.tf
@@ -1,21 +1,21 @@
 output "fingerprints" {
   description = "The fingerprints of the SSH keys."
   value       = {
-    for k,v in digitalocean_ssh_key.this: v.fingerprint
+    for k,v in digitalocean_ssh_key.this: k => v.fingerprint
   }
 }
 
 output "ids" {
   description = "The unique IDs of the SSH keys."
   value       = {
-    for k,v in digitalocean_ssh_key.this: v.id
+    for k,v in digitalocean_ssh_key.this: k => v.id
   }
 }
 
 output "keys" {
   description: "The SSH keys"
   value = {
-    for k,v in digitalocean_ssh_key.this: {
+    for k,v in digitalocean_ssh_key.this: k => {
       "fingerprint": v.fingerprint,
       "id": v.id,
       "name": v.name
@@ -27,13 +27,13 @@ output "keys" {
 output "names" {
   description = "The names of the SSH keys."
   value       = {
-    for k,v in digitalocean_ssh_key.this: v.name
+    for k,v in digitalocean_ssh_key.this: k => v.name
   }
 }
 
 output "public_keys" {
   description = "The text of the public SSH keys."
   value       = {
-    for k,v in digitalocean_ssh_key.this: v.public_key
+    for k,v in digitalocean_ssh_key.this: k => v.public_key
   }
 }
