5f774df0d56cbc138cdcf7c069f9a11edca64107
Olivier Pichon <op@dzango.com>
2022-04-02 17:00:36 +0700

    fix[ssh_key]: add support for multiple keys

--- ssh-key/variables.tf
+++ ssh-key/variables.tf
@@ -3,12 +3,10 @@ variable "digitalocean_token" {
   type        = string
 }
 
-variable "name" {
-  description = "The name of the SSH key for identification."
-  type        = string
-}
-
-variable "public_key" {
-  description = "The public key. If this is a file, it can be read using the file interpolation function."
-  type        = string
+variable "keys" {
+  description = "The keys to be created."
+  type        = list(object{
+    name = string
+    public_key = string
+  })
 }
