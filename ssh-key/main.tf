07f5074406e32ffa1a02b102efc8364f0d5bb51a
Olivier Pichon <op@dzango.com>
2022-04-02 17:12:37 +0700

    fix[ssh_key]: add support for multiple keys

--- ssh-key/main.tf
+++ ssh-key/main.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_ssh_key" "this" {
   for_each = { for key in var.keys : key.name => key }
 
-  name       = each.value.key
+  name       = each.value.name
   public_key = file(each.value.public_key)
 }
