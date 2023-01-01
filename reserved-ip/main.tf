b8386e8297dd3466a7de3a6bdc60d1da1a254792
Olivier Pichon <op@dzango.com>
2023-01-01 20:36:50 +0700

    fix(reserved-ip): fix module

--- reserved-ip/main.tf
+++ reserved-ip/main.tf
@@ -12,19 +12,3 @@ resource "digitalocean_project_resources" "reserved_ips" {
     digitalocean_reserved_ip.this[count.index].urn
   ]
 }
-
-resource "digitalocean_reserved_ip_assignment" "reserved_ip" {
-  count      = var.droplet_id != null ? 1 : 0
-  depends_on = [digitalocean_reserved_ip.this]
-  droplet_id = var.droplet_id
-  ip_address = digitalocean_reserved_ip.this.ip_address
-
-  provisioner "local-exec" {
-    command = "ssh-keyscan ${self.ip_address} 2>&1 | grep -vE '^#' >> ~/.ssh/known_hosts"
-  }
-
-  provisioner "local-exec" {
-    when    = destroy
-    command = "sed -i -e '/^${self.ip_address} .*/d' ~/.ssh/known_hosts"
-  }
-}
