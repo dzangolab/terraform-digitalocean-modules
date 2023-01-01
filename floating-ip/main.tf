48fc8bc6f0c29f445e8067b4b5cd364402c8c8b8
Olivier Pichon <op@dzango.com>
2023-01-01 20:00:25 +0700

    feat(floating_ip): add flaoting_ip_assignment resource

--- floating-ip/main.tf
+++ floating-ip/main.tf
@@ -1,8 +1,6 @@
 resource "digitalocean_floating_ip" "this" {
   count = var.ip_count
 
-  droplet_id = var.droplet_id
-
   region = var.region
 }
 
@@ -13,4 +11,20 @@ resource "digitalocean_project_resources" "floating_ips" {
   resources = [
     digitalocean_floating_ip.this[count.index].urn
   ]
+}
+
+resource "digitalocean_floating_ip_assignment" "floating_ip" {
+  count      = var.droplet_id != null ? 1 : 0
+  depends_on = [digitalocean_floating_ip.this]
+  droplet_id = var.droplet_id
+  ip_address = digitalocean_floating_ip.this.ip_address
+
+  provisioner "local-exec" {
+    command = "ssh-keyscan ${self.ip_address} 2>&1 | grep -vE '^#' >> ~/.ssh/known_hosts"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sed -i -e '/^${self.ip_address} .*/d' ~/.ssh/known_hosts"
+  }
 }
