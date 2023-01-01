681f1bf0d417894fbce91e8d6b24880353a68b0a
Olivier Pichon <op@dzango.com>
2023-01-01 21:16:39 +0700

    fix(reserved-ip): remove project_resource

--- reserved-ip/main.tf
+++ reserved-ip/main.tf
@@ -3,12 +3,3 @@ resource "digitalocean_reserved_ip" "this" {
 
   region = var.region
 }
-
-resource "digitalocean_project_resources" "reserved_ips" {
-  count      = var.project_id != null ? var.ip_count : 0
-  depends_on = [digitalocean_reserved_ip.this]
-  project    = var.project_id
-  resources = [
-    digitalocean_reserved_ip.this[count.index].urn
-  ]
-}
